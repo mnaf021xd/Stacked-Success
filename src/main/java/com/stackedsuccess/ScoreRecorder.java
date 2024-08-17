d3295a3e9c9a77acfe593378cdfe20fe34a0f2e4
mnaf021xd <100335102+mnaf021xd@users.noreply.github.com>
2024-08-17 18:22:50 +1200

    Update ScoreRecorder.java
    fixed sonarcloud

--- src/main/java/com/stackedsuccess/ScoreRecorder.java
+++ src/main/java/com/stackedsuccess/ScoreRecorder.java
@@ -7,13 +7,8 @@ public class ScoreRecorder {
     private static final String scoreFile = "score.txt";
 
     public static void saveScore(String score) {
-        try (FileWriter writer = new FileWriter(scoreFile)) {
+        FileWriter writer = new FileWriter(scoreFile)) {
             writer.write(score);
-        }
-
-        catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
 
